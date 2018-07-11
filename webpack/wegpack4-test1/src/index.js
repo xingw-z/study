8e364b1f095a057e0685b956807d7f0670d8c182
xx <zouxingwei@dafy.com>
2018-07-11 16:26:01 +0800

    webpack4 初试

--- webpack/wegpack4-test1/src/index.js
+++ webpack/wegpack4-test1/src/index.js
@@ -1,1 +1,14 @@
-console.log('i am a entry point')
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>react demo</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/> , document.getElementById('root'));
--- webpack/wegpack4-test1/webpack.config.js
+++ webpack/wegpack4-test1/webpack.config.js
@@ -1,18 +1,65 @@
 const path = require('path')
-module.exports = {
-    entry: [
-        "babel-polyfill",
-        path.join(__dirname, './src/index.js')
-    ],
-    module:{
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+const MinCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production'
+    return {
+        entry: [
+            "babel-polyfill",
+            path.join(__dirname, './src/index.js')
+        ],
+        devServer: {
+            port: 3000
+        },
+        module:{
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }]
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        devMode ? 'style-loader' : MinCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'less-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader'
+                        }
+                    ]
                 }
-            }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebPackPlugin({
+                template: './public/index.html',
+                filename: './index.html'
+            }),
+            new MinCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            })
+            
         ]
     }
 }
