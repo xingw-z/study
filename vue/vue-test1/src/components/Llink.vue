5f9758dd312b63c4f53acb12e1453010750991b5
xingw-z <as26623111@outlook.com>
2018-07-26 16:29:05 +0800

    add slot

---
+++ vue/vue-test1/src/components/Llink.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="llink">
+    <a
+      v-bind:href="url"
+      class="nav-link"
+    >
+      <slot></slot>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'llink',
+  props: {
+    url: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
