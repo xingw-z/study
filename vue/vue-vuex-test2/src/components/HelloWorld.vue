5c63f24e5f472c052fc797486e5b0bd65609a1a8
xx <zouxingwei@dafy.com>
2018-07-31 15:24:36 +0800

    add vue-vuex-test2

---
+++ vue/vue-vuex-test2/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    HelloWorld
+    <router-link to="/Nanshou">Nanshou</router-link>
+    <div>{{ count }}</div>
+    <button @click="add">store.state.count++</button>
+    <div>{{ this.$store.getters.doneTodos }}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  props: {
+    msg: String
+  },
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    add () {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
