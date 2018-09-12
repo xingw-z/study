f20b3af714bb138ed63aabb99c56e20524b9b342
xingw-z <as26623111@outlook.com>
2018-09-12 17:35:55 +0800

    ts 进行时

---
+++ vue/vue-ts-test1/src/views/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todolist">
+    {{ test }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    // HelloWorld,
+  },
+})
+
+export default class TodoList extends Vue {
+  private test = 'test';
+
+
+  private mounted() {
+    console.log(123);
+    interface Person {
+      test: string;
+    }
+  }
+}
+</script>
