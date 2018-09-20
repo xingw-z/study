3e3bb6f95b53ced01f34b90e9bc26526cde45364
xingw-z <as26623111@outlook.com>
2018-09-20 10:24:11 +0800

    vue-cli 3.0 ts 简单todolist

---
+++ vue/vue-ts-test2/src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="TodoItem">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {},
+})
+export default class TodoItem extends Vue {
+  @Prop() private msg!: string;
+
+  private mounted() {
+    console.log(123);
+  }
+}
+</script>
---
+++ vue/vue-ts-test2/src/views/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="TodoList">
+    <TodoItem v-for="item in list" :key="item.key" :msg="item.msg"></TodoItem>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TodoItem from '@/components/TodoItem.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    TodoItem,
+  },
+})
+export default class TodoList extends Vue {
+  private list = [
+    {
+      key: 1,
+      msg: 'asd',
+    },
+    {
+      key: 2,
+      msg: 'asd',
+    },
+  ];
+}
+</script>
