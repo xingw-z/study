4fe2dab2892b58db9c493176be527e9810b9bc76
xingw-z <as26623111@outlook.com>
2019-01-29 11:13:36 +0800

    飞机大战 写到一半

--- cocos/Fighter/assets/scripts/count.js
+++ cocos/Fighter/assets/scripts/count.js
@@ -27,48 +27,38 @@ cc.Class({
         //         this._bar = value;
         //     }
         // },
-        far_bg: [cc.Node],  //用于管理背景图片结点的数组,记得回cocos面板中添加数组的结点数量
-
-        bg_speed: 0,   //移动时控制速度的变量
-
-        defaultY: 0,    //  默认的y值
-
-        logVal: 0
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        cc.debug.setDisplayStats(false)
+        this._count = cc.find('Canvas/Main/count');
+        var label = this._count.getComponent(cc.Label);
+        // console.log(label);
+        this._count.opacity = 0;
+        // var test = () => {
+        //     var _time = 0.5;
+        //     var _big = cc.spawn(cc.scaleTo(_time, 1.16), cc.fadeIn(_time)).easing(cc.easeIn(3.0));
+        //     var _small = cc.scaleTo(_time, 0.95).easing(cc.easeIn(3.0));
+        //     var _zh = cc.spawn(cc.scaleTo(_time, 0.3), cc.fadeOut(_time)).easing(cc.easeIn(3.0));
+        //     var _callFunc = cc.callFunc(() => {
+        //         var _num = parseInt(label.string);
+        //         label.string = _num - 1;
+        //     }, this);
+        //     this._count.runAction(cc.repeat(cc.sequence(_small, _big, _zh, _callFunc), 3));
+        // }
+        // test();
+
+        // this.schedule(() => {
+        //     this._count.opacity = 255;
+        //     label.string = 3;
+        //     test();
+        // }, 5);
     },
 
     start () {
-
+        
     },
 
-    update (dt) {
-        this.bgMove(this.far_bg, this.bg_speed);
-    },
-
-    bgMove (bgList, speed) {
-        //每次循环二张图片一起滚动
-        for (var index = 0; index < bgList.length; index++) {
-
-            bgList[index].y -=speed;
-        
-        }
-        
-        //y坐标减去自身的height得到这张背景刚好完全离开场景时的y值
-        
-        if (bgList[0].y <= 0 - this.defaultY) {
-        
-            bgList[0].y = this.defaultY; //离开场景后将此背景图的y重新赋值，位于场景的上方
-        
-        }
-        
-        if (bgList[1].y <= 0 - this.defaultY) {
-            bgList[1].y = this.defaultY;
-        
-        }
-    }
+    // update (dt) {},
 });
