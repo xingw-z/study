5adfabad0e9d4ebb0143216b5327da7cce45c4f5
zouxingwei <as26623111@163.com>
2019-03-11 09:59:54 +0800

    ts 学习中

---
+++ ts/10w/20190308/interface.ts
@@ -0,0 +1,26 @@
+interface couterInstance{
+    start: number;
+}
+
+interface Counter {
+    (start: number): couterInstance;
+    interval: number;
+    reset(): void;
+}
+
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) {
+        console.log('start is ' + start)
+        this.start = start;
+    };
+    counter.interval = 123;
+    counter.reset = function () {console.log('do you want reset counter?')};
+    return counter;
+}
+
+// let c = getCounter();
+// c(10);
+// c.reset();
+// c.interval = 5.0;
+
+// console.dir(c)
