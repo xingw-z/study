d127a1d7a9ed976546377fdf43ad4611b3d2a130
xingw-z <as26623111@outlook.com>
2019-03-15 17:24:16 +0800

    nest.js 从入门到放弃

---
+++ node/nest/ajax/ajax/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    hello
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  created() {
+    axios.get('http://localhost:3000/cats').then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ node/nest/project/src/cats/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Res, Body, HttpStatus, HttpCode } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
