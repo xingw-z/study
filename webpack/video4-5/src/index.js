4c395cf551f7e3a2d1246b73b769097a05c57a5d
xingw-z <as26623111@outlook.com>
2019-07-10 22:19:33 +0800

    webpack video 4-5  SplitChunksPlugin

---
+++ webpack/video4-5/src/index.js
@@ -0,0 +1,32 @@
+// import _ from 'lodash';
+import react from 'react';
+// console.log(_.join(['a', 'b', 'c'], '**'), react.version);
+// console.log(_.join(['a', 'b', 'c'], '**'));
+console.log(react.version)
+
+// function getComponent () {
+//     return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+//         var ele = document.createElement('div');
+
+//         ele.innerHTML = _.join(['a', 'b', 'c'], '-');
+//         return ele
+//     })
+// }
+
+// getComponent().then(ele => {
+// 	document.body.appendChild(ele);
+// })
+
+// function getComponent() {
+// 	return import('lodash').then(({ default: _ }) => {
+// 		var element = document.createElement('div');
+// 		element.innerHTML = _.join(['Dell', 'Lee'], '-');
+// 		return element;
+// 	})
+// }
+
+
+// getComponent().then(element => {
+// 	document.body.appendChild(element);
+// });
+
