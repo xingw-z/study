0bf8736abcca509c4eabb63d9453a5b74e93138e
zouxingwei <as26623111@163.com>
2019-07-10 19:51:13 +0800

    小测试

--- node/test/test1.js
+++ node/test/test1.js
@@ -1,4 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.end('asd');
-}).listen(8080)
+}).listen(8080);
+
+var querystring = require('querystring');
+console.log(JSON.stringify(querystring))
