689688e85f2e190199fb49d457a93b27cbbd2747
xingw-z <as26623111@outlook.com>
2019-07-14 02:12:03 +0800

    搞了点ts  模拟一下 书里 c的 结构体

---
+++ aha/ts/src/components/Queue.vue
@@ -0,0 +1,58 @@
+<template>
+ <div class="queue">queue</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  public mounted() {
+    interface queue {
+      data: Array<number>;
+      head: number;
+      tail: number;
+    }
+    let q :queue = {
+      data: [],
+      head: 1,
+      tail: 1,
+    }
+
+    q.data.length = 100;
+    q.head = 0;
+    q.tail = 0;
+    let list :Array<number> = [6, 3, 1, 7, 5, 8, 9, 2, 4]
+    // answar: 615947283
+    
+    for(let i :number = 0, j :number = list.length; i < j; i++) {
+      q.data[q.tail] = list[i];
+      q.tail++;
+    }
+    console.log(q.data);
+    
+    
+
+    let arr :Array<number> = [];
+
+    while(q.head < q.tail) {
+      // console.log(q.data[q.head]);
+      arr.push(q.data[q.head]);
+      q.head++;
+
+      q.data[q.tail] = q.data[q.head];
+      q.tail++;
+      q.head++;
+    }
+    console.log(arr);
+    
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- aha/ts/src/views/Home.vue
+++ aha/ts/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <Queue></Queue>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Queue from '@/components/Queue.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Queue,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private a :number = 123;
+  
+  public mounted() {
+    interface test {
+      x: number;
+      y: number;
+    }
+
+    let t :test = { x: 213, y: 123}
+    
+  }
+}
 </script>
