05f4a6ca74f78059e62273c8d5d3e90e14956c2e
zouxingwei <as26623111@163.com>
2019-07-14 16:34:28 +0800

    搞蒙了

--- aha/ts/src/components/Queue.vue
+++ aha/ts/src/components/Queue.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { log } from 'util';
 
 @Component
 export default class HelloWorld extends Vue {
@@ -56,6 +57,10 @@ export default class HelloWorld extends Vue {
       head: number;
       tail: number;
     }
+    interface stack {
+      data: Array<number>;
+      top: number;
+    }
     let q1 :queue = {
       data: [],
       head: 0,
@@ -66,9 +71,17 @@ export default class HelloWorld extends Vue {
       head: 0,
       tail: 0,
     }
+    let s :stack = {
+      data: [],
+      top: 0,
+    }
+
     let input1 :Array<number> = [2, 4, 1, 2, 5, 6];
+    // let input1 :Array<number> = [1, 2, 3, 4, 5];
     let input2 :Array<number> = [3, 1, 3, 5, 6, 4];
+    // let input2 :Array<number> = [6, 7, 8, 9];
     let i :number = 0;
+    let t :number;
 
     for (i = 0; i < input1.length; i++) {
       q1.data.push(input1[q1.tail++]);
@@ -78,6 +91,84 @@ export default class HelloWorld extends Vue {
       q2.data.push(input2[q2.tail++]);
     }
     
+    let book :Array<number> = [];
+    for (i = 0; i < 10; i++) {
+      book[i] = 0;
+    }
+    let start :any = new Date()
+
+
+    // while(q1.head < q1.tail && q2.head < q2.tail) {
+    while(q1.head < q1.tail) {
+      if (Date.now() - start > 1000) {
+        throw new Error('Loop running too long!')
+      }
+
+      t = q1.data[q1.head];
+      q1.head++;
+
+      if (!!book[t]) {
+        
+        q1.data[q1.tail] = t;
+        q1.tail++;
+        debugger
+        while(s.data[s.top-1] != t) {
+          console.log(q1, q2, s, book, t);
+             
+          if (Date.now() - start > 1000) {
+            throw new Error('Loop running too long!1')
+          }
+          book[s.data[s.top-1]] = 0;
+          q1.data[q1.tail] = s.data[s.top];
+          q1.tail++;
+          s.top--;
+        }
+        book[s.data[s.top-1]] = 0;
+        q1.data[q1.tail-1] = s.data[s.top];
+        q1.tail++;
+        s.top--;        
+        debugger
+      } else {
+        s.data[s.top] = t;
+        s.top++;
+        book[t]++;
+      }
+      if (q1.tail === q1.head) {
+        break;
+      };
+      console.log(s, q1, q2);
+      t = q2.data[q2.head];
+      q2.head++;
+      if (!!book[t]) {
+        q2.data[q2.tail] = t;
+        q2.tail++;
+        while(s.data[s.top-1] != t) {
+          debugger
+          if (Date.now() - start > 1000) {
+            throw new Error('Loop running too long!2')
+          }
+          book[s.data[s.top-1]] = 0;
+          q2.data[q2.tail] = s.data[s.top];
+          q2.tail++;
+          s.top--;
+        }
+        book[s.data[s.top-1]] = 0;
+        q2.data[q2.tail] = s.data[s.top];
+        q2.tail++;
+        s.top--;
+      } else {
+        s.data[s.top] = t;
+        s.top++;
+        book[t]++;
+      }
+
+      if (q2.tail === q2.head) {
+        console.log('q2');
+        break;
+      };
+
+    }
+    console.log(s, q1, q2, book);
     
 
   }
