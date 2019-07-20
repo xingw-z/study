3889922e97721dc9e35470f17aff0556d6d54d28
zouxingwei <as26623111@163.com>
2019-07-20 16:08:41 +0800

    wepback video 4-12 5-1

---
+++ webpack/video4-12/build/webpack.dev.js
@@ -0,0 +1,50 @@
+const meger = require('webpack-merge');
+const webpack = require('webpack');
+// const commonConfig = require('./webpack.common');
+
+const devConfig = {
+	mode: 'development',
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: './dist',
+		open: true,
+		port: 8080,
+		hot: true,
+		hotOnly: true
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	optimization: {
+		
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [
+				'style-loader', 
+				{
+					loader: 'css-loader',
+					options: {
+						importLoaders: 2
+					}
+				},
+				'sass-loader',
+				'postcss-loader'
+			]
+		}, {
+			test: /\.css$/,
+			use: [
+				'style-loader', 
+				'css-loader',
+				'postcss-loader'
+			]
+		}]
+	},
+	output: {
+		filename: '[name].js',
+		chunkFilename: '[name].chunk.js',
+	}
+}
+
+module.exports = devConfig;
---
+++ webpack/video5-1-library/src/math.js
@@ -0,0 +1,15 @@
+export function jia(a, b) {
+    return a + b;
+}
+
+export function jian(a, b) {
+    return a - b;
+}
+
+export function cheng(a, b) {
+    return a * b;
+}
+
+export function chu(a, b) {
+    return a / b;
+}
