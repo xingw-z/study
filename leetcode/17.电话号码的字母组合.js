5de309a1059b7964d7ffa7ffc404b6614ac634db
xingw-z <as26623111@outlook.com>
2019-07-31 16:42:38 +0800

    好久没做 题了 做一题醒醒脑

--- leetcode/17.电话号码的字母组合.js
+++ leetcode/17.电话号码的字母组合.js
@@ -8,6 +8,39 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    let phone = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    }
     
+    let output = [];
+
+    function backtrack(zh, next_digits) {
+        if (next_digits.length === 0) {
+            output.push(zh)
+        } else {
+            let digit = next_digits.substring(0, 1);
+            let letters = phone[digit];
+            for(let i = 0; i < letters.length; i++) {
+                let letter = phone[digit].substring(i, i + 1);
+                backtrack(zh + letter, next_digits.substring(1));
+            }
+        }
+    }
+
+    function func(d) {
+        if (d.length != 0) {
+            backtrack('', d)
+        }
+        return output;
+    }
+    
+    return func(digits);
 };
 
