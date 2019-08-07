c4ccb26bd1dcd5d6aa7ca3d61a576cf4b015ca0a
zouxingwei <as26623111@163.com>
2019-08-07 15:00:51 +0800

    leetcode 18题 类似三数之和的 双指针 但是 前边套了两个循环 假如 五数之和我不就懵逼了

--- leetcode/18.四数之和.js
+++ leetcode/18.四数之和.js
@@ -9,6 +9,32 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-    
+    let val = nums[0] + nums[1] + nums[2] + nums[3];
+    let i = 0;
+    let len = nums.length;
+    let list = [];
+    let qsort = arr => arr.length > 1 ? [...qsort(arr.filter(item => item < arr[0])), ...arr.filter(item => item === arr[0]), ...arr.filter(item => item > arr[0])] : arr;
+    nums = qsort(nums);
+    while(i < len - 2) {
+        let j = i + 1;
+        while(j < len - 2) {
+            let start = j + 1;
+            let end = len - 1;
+            while(start < end) {
+                let temp = nums[i] + nums[j] + nums[start] + nums[end];    
+                if (temp === target) {
+                    list.push([nums[i], nums[j], nums[start], nums[end]])
+                }
+                if (temp < target) {
+                    while(nums[start] === nums[++start]);
+                } else {
+                    while(nums[end] === nums[--end]);
+                }
+            }
+            while(nums[j] === nums[++j]);
+        }
+        while(nums[i] === nums[++i]);
+    }
+    return list;
 };
 
