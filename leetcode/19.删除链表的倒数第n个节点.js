722008d54741b774c7252864bef40852cfbd602e
xingw-z <as26623111@outlook.com>
2019-08-08 09:11:21 +0800

    leetcode 做出19题 卡在 22题了

---
+++ leetcode/19.删除链表的倒数第n个节点.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第N个节点
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+};
+
---
+++ leetcode/22.括号生成.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=javascript
+ *
+ * [22] 括号生成
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+};
+
