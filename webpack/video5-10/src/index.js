3871b00b26d75943d20431b26ecbac601fa6ad3c
zouxingwei <as26623111@163.com>
2019-08-28 21:13:33 +0800

    webpack video 5-10

---
+++ webpack/video5-10/src/index.js
@@ -0,0 +1,17 @@
+// console.log(this === window);
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import _ from 'lodash';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<div>{_.join(['this', 'is', 'app'], ' ')}</div>
+			</div>
+		)
+	}
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
