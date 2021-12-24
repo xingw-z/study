f58526c59d7703f75578f9a0e9ad4d0a7cb3aef2
xingw-z <as26623111@outlook.com>
2021-12-24 11:57:30 +0800

    feat: 暂存

--- test/react-test/src/pages/quill/index.tsx
+++ test/react-test/src/pages/quill/index.tsx
@@ -1,15 +1,29 @@
 import styles from './index.less';
 import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
 import { useEffect } from 'react';
+import Toolbar from './Toolbar';
 
 
 export default function IndexPage() {
   useEffect(() => {
-    // const 
+    const quill = new Quill('.eee', {
+      modules: {
+        table: true,
+        toolbar: {
+          container: '.ttt'
+        }
+      },
+      theme: 'snow',
+    })
   });
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>quill page</h1>
+      <div className="ccc">
+        <Toolbar className="ttt" />
+        <div className="eee"></div>
+      </div>
     </div>
   );
 }
